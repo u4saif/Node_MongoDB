dc6d2e7b7b349bde615f2fe9cbe899de98487b5a
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-05-02 16:14:35 +0530

    API server with http module

--- DevCamper_API/server.js
+++ DevCamper_API/server.js
@@ -1,12 +1,30 @@
-const http = require('http');
+//-------With HTTP module-------------------
+// const http = require('http');
 
-PORT = 5000;
-const server = http.createServer((req, res) => {
-    const { headers, url, method } = req;
-    console.log({ headers, url, method });
-    res.end();
-})
+// PORT = 5000;
+// const server = http.createServer((req, res) => {
+//     const { headers, url, method } = req;
+//     console.log({ headers, url, method });
+//     res.setHeader("Content-Type", "text/html")
+//     res.write('<h1>Hello from NodeJs and MongoDB</h1>');
+//     res.end();
+// })
 
-server.listen(PORT, () => {
-    console.log(`API Server Started at Port:-${PORT}`);
+// server.listen(PORT, () => {
+//     console.log(`API Server Started at Port:-${PORT}`);
+// })
+
+//-------------------With Express--------------------
+
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send("API with Node and Express");
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port: ", PORT);
 })
